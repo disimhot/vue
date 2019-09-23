0837ddc757fe9ece40bd5d3b2ad4cce8fddc49d6
efimova <efimova@betweenx.com>
2019-09-23 09:58:35 +0300

    added files

---
+++ vue-proj/src/views/AddUser.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h5 class="display-4">Add user</h5>
+    <div v-if="id">
+        <user-form  v-model="user" />
+        <button type="button" class="btn btn-primary" @click="saveUser">Save</button>
+    </div>
+
+    <div class="alert alert-warning" role="alert" v-else
+      >Загрузка</div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Add',
+  components: {
+    'user-form': () => import('@/components/UserForm.vue')
+  },
+  data: function() {
+    return {
+      user: {},
+      id: null,
+    }
+  },
+  computed: {
+    url() {
+      return 'http://localhost:3000/users/';
+    }
+  },
+  mounted() {
+    axios.get(this.url)
+      .then(response => response.data)
+      .then(users => {
+          this.id = users.length + 1;
+      })
+    .catch(error => {
+      console.log('Error ' + error);
+    })
+  },
+  methods: {
+    saveUser() {
+      axios
+        .put(this.url + this.id, this.user)
+        .then(() => {
+          this.$router.push('/users')
+        })
+        .catch(error => {
+          console.log('Error ' + error)
+        })
+    }
+  }
+}
+</script>
---
+++ vue-proj/src/views/NotFoundPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+    <div class="d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="error-template">
+                <h1 class="mt-5">
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions m-5">
+                    <router-link to="/" exact>
+                      <a class="btn btn-outline-primary btn-lg">Take Me Home</a>
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'NotFound'
+}
+</script>
